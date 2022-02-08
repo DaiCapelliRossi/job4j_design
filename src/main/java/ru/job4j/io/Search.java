bb7d9f837027f2663f6fb38ab09499e407054b8b
DaiCapelliRossi <ness.ostroumova@gmail.com>
2022-02-08 20:54:59 +0300

    11.5. Валидация параметров запуска [00029]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,8 +9,14 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".class")).forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null & Search parameter is null. Usage java -jar search.jar ROOT_FOLDER, FILE_EXTENSION.");
+        }
+        Path start = Paths.get(args[0]);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Search parameter is null. Usage java -jar search.jar FILE_EXTENSION.");
+        }
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
