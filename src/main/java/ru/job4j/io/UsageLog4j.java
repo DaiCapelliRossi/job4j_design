b84c677b7e7453aa779cd4b6f7cda26574bd0826
DaiCapelliRossi <ness.ostroumova@gmail.com>
2022-05-09 16:05:25 +0300

    13.3. Slf4j - вывод переменных [00038]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -13,5 +13,19 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+
+        String name = "Anastasia";
+        byte age = 27;
+        char sex = 'F';
+        float weight = 58.20f;
+        double height = 169.09;
+        boolean hasJob = true;
+        long salary = 100000L;
+        short work_exp = 7;
+        int savings = 200000;
+
+        LOG.debug("User Info name: {}, age: {}, sex: {}, weight: {}, height: {}, hasJob: {}, salary: {}, work_exp: {}, savings: {}",
+                name, age, sex, weight, height, hasJob, salary, work_exp, savings);
+
     }
 }
